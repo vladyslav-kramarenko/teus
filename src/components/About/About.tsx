5e6f152234e2b18442e4a0c9a36663361b099497
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-08-12 16:49:38 -0700

    created about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,12 +1,24 @@
-// src/components/About.tsx
 import React from 'react';
+import './About.css';
+import aboutImage from '../../assets/images/slider1_4.png'; // Update with the correct image path
 
 const About: React.FC = () => {
     return (
-        <section id="about" className="about-us">
-            <h2>About Us</h2>
-            <p>Teus Group is an investment and construction company that creates breakthrough projects of the future.</p>
-        </section>
+        <div className="about-container">
+            <div className="about-header">
+                <h2>About Us</h2>
+                <a href="#contact" className="action-button">Reach Us</a>
+            </div>
+            <div className="about-content">
+                <p>
+                    TEUS GROUP IS AN <span className={"highlight"}>INVESTMENT AND CONSTRUCTION COMPANY</span> THAT CREATES BREAKTHROUGH PROJECTS OF THE FUTURE.
+                    THE GLOBAL VISION IS TO PROVIDE CLIENTS <span className={"highlight"}>INNOVATIVE ARCHITECTURAL SOLUTIONS</span> WITH THOUGHTFUL DESIGN AND INTERIOR CONCEPTS AT COMPETITIVE PRICES.
+                </p>
+            </div>
+            <div className="about-image">
+                <img src={aboutImage} alt="About Us" />
+            </div>
+        </div>
     );
 }
 
