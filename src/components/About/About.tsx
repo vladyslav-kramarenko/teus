85d1166f0a2e02aacb97468f0737e2366eeb5cb3
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-08-13 16:40:50 -0700

    some style changes in about section

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,19 +4,23 @@ import aboutImage from '../../assets/images/slider1_4.png'; // Update with the c
 
 const About: React.FC = () => {
     return (
-        <div className="about-container">
-            <div className="about-header">
-                <h2>About Us</h2>
-                <a href="#contact" className="action-button">Reach Us</a>
-            </div>
-            <div className="about-content">
-                <p>
-                    TEUS GROUP IS AN <span className={"highlight"}>INVESTMENT AND CONSTRUCTION COMPANY</span> THAT CREATES BREAKTHROUGH PROJECTS OF THE FUTURE.
-                    THE GLOBAL VISION IS TO PROVIDE CLIENTS <span className={"highlight"}>INNOVATIVE ARCHITECTURAL SOLUTIONS</span> WITH THOUGHTFUL DESIGN AND INTERIOR CONCEPTS AT COMPETITIVE PRICES.
-                </p>
-            </div>
-            <div className="about-image">
-                <img src={aboutImage} alt="About Us" />
+        <div className="about-bg-container">
+            <div className="about-container">
+                <div className="about-header">
+                    <h2>About Us</h2>
+                    <a href="#contact" className="action-button">Reach Us</a>
+                </div>
+                <div className="about-content">
+                    <p>
+                        TEUS GROUP IS AN <span className={"highlight"}>INVESTMENT AND CONSTRUCTION COMPANY</span> THAT
+                        CREATES BREAKTHROUGH PROJECTS OF THE FUTURE.
+                        THE GLOBAL VISION IS TO PROVIDE CLIENTS <span className={"highlight"}>INNOVATIVE ARCHITECTURAL SOLUTIONS</span> WITH
+                        THOUGHTFUL DESIGN AND INTERIOR CONCEPTS AT COMPETITIVE PRICES.
+                    </p>
+                </div>
+                <div className="about-image">
+                    <img src={aboutImage} alt="About Us"/>
+                </div>
             </div>
         </div>
     );
