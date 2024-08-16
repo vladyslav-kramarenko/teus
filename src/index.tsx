4905e516b14f5f1330062ddcc53154cc6f797dc2
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-08-15 23:03:48 -0700

    added gtm script

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import insertGtmScript from "./util/Gtm";
+
+insertGtmScript();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
