8a0844313ee84ad58bc8c5a3ed6267c1fae8c250
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-08-15 22:56:13 -0700

    added hubspot integration

--- src/components/ReachUs/ReachUs.tsx
+++ src/components/ReachUs/ReachUs.tsx
@@ -1,9 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './ReachUs.css';
-import titleImage from '../../assets/images/reach_us.webp'; // Replace with your title image path
+import titleImage from '../../assets/images/reach_us.webp';
+import ModalMessage from "../ModalMessage/ModalMessage";
+// import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import { createContact, createDeal } from '../../util/hubspotApi';
+import { saveUTMParams } from "../../util/saveUTMParams";
 
 const ReachUs: React.FC = () => {
-    const [formData, setFormData] = useState({
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalMessage, setModalMessage] = useState('');
+    const [formData, setFormData] = useState<{
+        name: string;
+        phone: string | undefined;
+        message: string;
+    }>({
         name: '',
         phone: '',
         message: '',
@@ -16,38 +27,99 @@ const ReachUs: React.FC = () => {
         });
     };
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        console.log('Form submitted:', formData);
+        setModalMessage("Thank You for Your Message! Our manager will contact you soon");
+        setIsModalOpen(true);
+        const utmParams = saveUTMParams();  // Get the saved UTM parameters
+
+        const contactData = {
+            firstname: formData.name,
+            phone: formData.phone,
+            hs_language: "en",
+            website: "teus-group.com",
+            lifecyclestage: 'lead',
+            ...utmParams,  // Include UTM parameters in the contact data
+        };
+
+        try {
+            // Create Contact
+            const contactResponse = await createContact(contactData);
+            const contactId = contactResponse.id;
+
+            // Create Deal associated with the Contact
+            const dealData = {
+                properties: {
+                    dealname: formData.name,
+                    dealstage: '522672623',
+                    pipeline: '330883560',
+                    lead_source: 'site - teus-group.com',
+                    comment: formData.message,
+                    ...utmParams,  // Include UTM parameters in the deal data
+                }
+            };
+
+            await createDeal(contactId, dealData);
+            setModalMessage("Thank You for Your Message! Our manager will contact you soon");
+            setIsModalOpen(true);
+            console.log('Contact and Deal created successfully');
+        } catch (error) {
+            console.error('Error creating contact or deal:', error);
+        }
+
     };
 
     return (
         <section id="contact" className="contact-form">
-            <img src={titleImage} alt="Reach Us" className="contact-form-title"/>
+            <img src={titleImage} alt="Reach Us" className="contact-form-title" />
             <div className={"contact-form-container"}>
                 <p className={"contact-form-description"}>
-                    Highly skilled teams together
-                    <br/>
-                    with long-term` proven
-                    <br/>
-                    suppliers
-                    enable them to create
-                    <br/>projects of the
-                    latest
-                    <br/>standards: architectural excellence, energy-efficient design, sustainable construction
-                    materials,
-                    smart home features, green spaces, amenities, high-quality finishes and many others.</p>
+                    Highly skilled teams together with long-term proven suppliers enable them to create projects of the
+                    latest standards: architectural excellence, energy-efficient design, sustainable construction materials,
+                    smart home features, green spaces, amenities, high-quality finishes and many others.
+                </p>
                 <form onSubmit={handleSubmit}>
                     <input type="text" name="name" placeholder="NAME" onChange={handleChange} value={formData.name}/>
                     <input type="text" name="phone" placeholder="PHONE" onChange={handleChange} value={formData.phone}/>
-                    <input type="text" name="message" placeholder="MESSAGE" onChange={handleChange} value={formData.message}/>
-                    {/*<textarea name="message" placeholder="Message" onChange={handleChange}*/}
+                    <input type="text" name="message" placeholder="message" onChange={handleChange} value={formData.message}/>
+                    {/*<PhoneInput*/}
+                    {/*    placeholder="PHONE"*/}
+                    {/*    value={formData.phone}*/}
+                    {/*    onChange={(value) => setFormData({...formData, phone: value})}*/}
+                    {/*/>*/}
+                    {/*<textarea name="message" placeholder="MESSAGE" onChange={handleChange}*/}
                     {/*          value={formData.message}></textarea>*/}
                     <button type="submit">Send & Get Feedback</button>
                 </form>
+                <ModalMessage
+                    isOpen={isModalOpen}
+                    message={modalMessage}
+                    onClose={() => setIsModalOpen(false)}
+                />
             </div>
         </section>
     );
 }
 
 export default ReachUs;
+
+
+// <input
+//     type="text"
+//     // placeholder={intl.formatMessage({id: 'name'})}
+//     placeholder='name'
+//     // maxLength='20'
+//     value={name}
+//     onChange={e => setName(e.target.value)}
+// />
+//
+// <PhoneInput
+//     international
+//     // defaultCountry={intl.locale === "en" ? "US" : intl.locale.toString().toUpperCase()}
+//     defaultCountry="US"
+//     value={phone}
+//     onChange={setPhone}
+//     // placeholder={intl.formatMessage({id: 'phone'})}
+//     placeholder="phone"
+//     // labels={labels[intl.locale]}
+// />
---
+++ src/util/hubspotApi.js
@@ -0,0 +1,45 @@
+// src/services/hubspotApi.js
+
+import axios from 'axios';
+
+const PROXY_BASE_URL = 'https://proxy.teus-group.com/api/hubspot/crm/v3/objects';
+
+export const createContact = async (data) => {
+    try {
+        const response = await axios.post(
+            `${PROXY_BASE_URL}/contacts`,
+            { properties: data }
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error creating contact:', error);
+        throw error;
+    }
+};
+
+export const createDeal = async (contactId, dealData) => {
+    try {
+        const contactToDealAssociation = '3';
+        const response = await axios.post(
+            `${PROXY_BASE_URL}/deals`,
+            {
+                properties: dealData.properties,
+                associations: [
+                    {
+                        to: { id: contactId },
+                        types: [
+                            {
+                                associationCategory: 'HUBSPOT_DEFINED',
+                                associationTypeId: contactToDealAssociation
+                            }
+                        ]
+                    }
+                ]
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error('Error creating deal:', error);
+        throw error;
+    }
+};
