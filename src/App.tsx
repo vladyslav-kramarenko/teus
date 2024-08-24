f7c68c36483c79ef34b1853eac35f27497361c6b
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-08-23 19:45:25 -0700

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,36 @@
-// src/App.tsx
 import React, {useEffect} from 'react';
 import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import About from './components/About/About';
-import ReachUs from './components/ReachUs/ReachUs';
 import Footer from './components/Footer/Footer';
-import HistoryAndBackground from "./components/HistoryAndBackground/HistoryAndBackground";
-import DesignComponent from "./components/Design/DesignComponent";
-import CEO from "./components/CEO/CEO";
-import GoldenStandards from "./components/GoldenStandarts/GoldenStandarts";
-import Projects from "./components/Projects/Projects";
-import Partners from "./components/Partners/Partners";
+
 import {saveUTMParams} from "./util/saveUTMParams";
 import './App.css';
+import HomePage from "./pages/HomePage";
+import {Route, BrowserRouter as Router, Routes, Navigate} from "react-router-dom";
+import NewsPage from "./pages/NewsPage/NewsPage";
+import newsData from "./data/newsData";
 
 const App: React.FC = () => {
     useEffect(() => {
         saveUTMParams();
     }, []);
+
+    const latestNewsId = newsData[newsData.length - 1].id;
+
+
     return (
-        <div className="App">
-            <Header/>
-            <main>
-                <Hero/>
-                <About/>
-                <HistoryAndBackground/>
-                <DesignComponent/>
-                <CEO/>
-                <GoldenStandards/>
-                <Projects/>
-                <Partners/>
-                <ReachUs/>
-            </main>
-            <Footer/>
-        </div>
+        <Router>
+            <div className="App">
+                <Header/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/news" element={<Navigate to={`/news/${latestNewsId}`} replace />} />
+                        <Route path="/news/:newsId" element={<NewsPage/>}/>
+                    </Routes>
+                </main>
+                <Footer/>
+            </div>
+        </Router>
     );
 }
 
