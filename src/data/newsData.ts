e225b1e50dee6fff989b0ac0ef74c901208fcb60
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-08-25 22:52:19 -0700

    news data update

--- src/data/newsData.ts
+++ src/data/newsData.ts
@@ -10,32 +10,53 @@ const newsData = [
             "In honour of the agreement, Teus Group has launched a special offer. 7 different apartments in the complex can be purchased with a 7% discount upon 100% payment.",
             "As a reminder, DESIRE is a new project by Teus Group. It is a premium class complex located in one of the most promising and fastest growing areas of Antalya. The building combines a residential complex and a hotel, which means that the project is aimed at both investors and those who want to buy their own home. The complex will be built by a well-known Turkish developer with over 30 years of experience in the Antalya construction market."
         ],
-        mainPhoto: "/images/Pidpisannya-dogovoru-z-BWH--scaled.jpg",
-        thumbnail: "/images/Pidpisannya-dogovoru-z-BWH--scaled.jpg"
+        mainPhoto: "/images/Pidpisannya-dogovoru-z-BWH--scaled_.jpg",
+        thumbnail: "/images/Pidpisannya-dogovoru-z-BWH--scaled_min.jpg"
     },
     {
         id: 2,
         title: "Launch of a new project by Teus Group - DESIRE premium class residential complex",
         date: "13 September 2023",
-        description: ["Description of the second news item."],
-        mainPhoto: "/images/Pidpisannya-dogovoru-z-BWH--scaled.jpg",
-        thumbnail: "/images/Pidpisannya-dogovoru-z-BWH--scaled.jpg"
+        description: [
+            "Teus Group is launching a new project in the Antalya market - the DESIRE premium class residential complex in Antalya. This is our first project in the Turkish real estate market.",
+            "What is DESIRE?",
+            "DESIRE is a premium class complex with a well-thought-out infrastructure and 5-star service, located in the most promising area of Antalya. The building combines a residential complex and an apart-hotel. Everyone will be able to choose what they need - a profitable investment project or their own home. Swimming pools, barbecue areas, cafes, a rooftop cinema, fitness and spa facilities - all these benefits are gathered in one complex.",
+            "The architectural project was created by the world-famous architectural firm AIMM. The company's specialists have created projects all over the world. Namely, in the UAE, Dominican Republic, China, and a number of European countries.",
+            "Details about the complex can be found on the official project website.",
+            "Follow the link: https://www.desire-antalya.com"
+        ],
+        mainPhoto: "/images/news_13.09.2023_.jpg",
+        thumbnail: "/images/news_13.09.2023_min.jpg"
     },
     {
         id: 3,
         title: "The foundation of the DESIRE residential complex has been installed",
         date: "15 April 2024",
-        description: ["Description of the second news item."],
-        mainPhoto: "/images/Pidpisannya-dogovoru-z-BWH--scaled.jpg",
-        thumbnail: "/images/Pidpisannya-dogovoru-z-BWH--scaled.jpg"
+        description: [
+            "The foundation of the DESIRE residential complex has been installed",
+            "Saturday was a remarkable day for us and our Turkish partners involved in the construction of the DESIRE residential complex. Within 28 hours, the foundation slab of the building was installed. Very soon we will be able to see how quickly the building will grow.",
+            "The premium class complex is located in sunny Antalya, near the sea in the youngest and most promising area of the city. The complex is scheduled to be commissioned in the fourth quarter of 2025.",
+            "Project website: https://desire-antalya.com",
+        ],
+        mainPhoto: "/images/news_15.04.2024_.jpg",
+        thumbnail: "/images/news_15.04.2024_min.jpg"
     },
     {
-        id: 1,
+        id: 4,
         title: "Investment in the DESIRE residential complex. Opportunity to purchase 1/4, 1/2 or 3/4 of an apartment",
         date: "17 May 2024",
-        description: ["Description of the second news item."],
-        mainPhoto: "/images/Pidpisannya-dogovoru-z-BWH--scaled.jpg",
-        thumbnail: "/images/Pidpisannya-dogovoru-z-BWH--scaled.jpg"
+        description: [
+            "You can buy an apartment in Turkey for rent even without having the full amount.",
+            "We are talking about our project, namely a premium residential complex in Antalya, Turkey. The complex consists of a part with residences and a hotel part. The rooms in the latter can be purchased as a whole or as ¼ or ½ or ¾ of a room.",
+            "The property management, i.e. all the hassle with tenants and minor repairs, is taken care of by the global hotel brand Best Western Inc. The partnership with one of the largest hotel chains in the world predicts a yield of up to 15% per annum with payments in euros. In this case, the investor only receives dividends, i.e. it is a completely passive income.",
+            "Why is such an investment profitable?",
+            "Diversification of the investment portfolio.",
+            "Lower minimum investment threshold.",
+            "A completely passive investment instrument.",
+            "Project website: https://desire-antalya.com"
+        ],
+        mainPhoto: "/images/news_17.05.2024_.jpg",
+        thumbnail: "/images/news_17.05.2024_min.jpg"
     },
 ];
 
